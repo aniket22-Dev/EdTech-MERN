a2e5a5980dee53ea03bd2bbdaece67c993af05f2
Aniket Bhatnagar <86366772+aniket22-Dev@users.noreply.github.com>
2023-06-25 13:06:13 +0530

    new integrations added

--- backend/controllers/Section.js
+++ backend/controllers/Section.js
@@ -1,5 +1,6 @@
 const Section = require("../models/Section");
 const Course = require("../models/Course");
+const SubSection = require("../models/SubSection");
 // CREATE a new section
 exports.createSection = async (req, res) => {
 	try {
@@ -54,15 +55,26 @@ exports.createSection = async (req, res) => {
 // UPDATE a section
 exports.updateSection = async (req, res) => {
 	try {
-		const { sectionName, sectionId } = req.body;
+		const { sectionName, sectionId,courseId } = req.body;
 		const section = await Section.findByIdAndUpdate(
 			sectionId,
 			{ sectionName },
 			{ new: true }
 		);
+
+		const course = await Course.findById(courseId)
+		.populate({
+			path:"courseContent",
+			populate:{
+				path:"subSection",
+			},
+		})
+		.exec();
+
 		res.status(200).json({
 			success: true,
 			message: section,
+			data:course,
 		});
 	} catch (error) {
 		console.error("Error updating section:", error);
@@ -93,7 +105,7 @@ exports.deleteSection = async (req, res) => {
 		}
 
 		//delete sub section
-		await SubSectionModal.deleteMany({_id: {$in: section.subSection}});
+		await SubSection.deleteMany({_id: {$in: section.subSection}});
 
 		await Section.findByIdAndDelete(sectionId);
 
@@ -111,19 +123,6 @@ exports.deleteSection = async (req, res) => {
 			message:"Section deleted",
 			data:course
 		});
-
-
-
-
-
-		//HW -> req.params -> test
-		const { sectionId } = req.params;
-		await Section.findByIdAndDelete(sectionId);
-		//HW -> Course ko bhi update karo
-		res.status(200).json({
-			success: true,
-			message: "Section deleted",
-		});
 	} catch (error) {
 		console.error("Error deleting section:", error);
 		res.status(500).json({
@@ -131,4 +130,4 @@ exports.deleteSection = async (req, res) => {
 			message: "Internal server error",
 		});
 	}
-};
+};   
