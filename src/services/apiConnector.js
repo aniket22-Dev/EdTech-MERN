46fa583bbaec4a3d3191d0af0858225b408c6661
Aniket Bhatnagar <86366772+aniket22-Dev@users.noreply.github.com>
2023-08-05 20:47:27 +0530

    added render api to base URl for Deployment

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000", // Change the base URL to http://localhost:4000
+  baseURL: "https://ed-tech-v1.onrender.com", // Change the base URL to http://localhost:4000
 });
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
